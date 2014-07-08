eb436fb1ea43bdca68fc39141e7c85040efe2e9b
Фукалов Семен <yapendalff@trioris.com>
2014-07-08 13:33:40 +0700

    Upd env files

--- environments/dev/index.php
+++ environments/dev/index.php
@@ -2,5 +2,5 @@
 require_once( __DIR__ . '/protected/bootstrap.php');
 require_once(YII_PATH . '/yii.php');
 
-$config = CONFIG_PATH.'/main.php';
+$config = YII_CONFIG.'/main.php';
 Yii::createWebApplication($config)->run();
